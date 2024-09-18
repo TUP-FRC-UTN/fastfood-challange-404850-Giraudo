001715d392056e6501c644754f72460314a85364
404850-Giraudo <404850@tecnicatura.frc.utn.edu.ar>
2024-09-18 01:38:36 -0300

    html de components

--- fast-food/src/app/app.component.ts
+++ fast-food/src/app/app.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { DeliveryPointComponent } from './delivery-point/delivery-point.component';
+import { KitchenComponent } from './kitchen/kitchen.component';
+import { PointOfSellComponent } from './point-of-sell/point-of-sell.component';
+import { RestaurantComponent } from './restaurant/restaurant.component';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, DeliveryPointComponent, KitchenComponent, PointOfSellComponent, RestaurantComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ fast-food/src/models/order-model.ts
@@ -0,0 +1,7 @@
+
+interface Order {
+    number: number; // Numero de pedido random de 1 a 1000
+    name: string;  // Nombre del cliente
+    description: string;  // Descripción del pedido
+    date: Date; // Fecha de creación del pedido
+}
